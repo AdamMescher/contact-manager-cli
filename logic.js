e44da3e08d55a2ca8cb46fd6f18e093e1826b070
Adam Mescher <admescher@gmail.com>
2018-01-28 18:13:06 -0700

    Write logic.js file

---
+++ logic.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const assert = require('assert');
+const db = mongoose.connect('mongodb://localhost:27017/contact-manager');
+mongoose.Promise = global.Promise;
+
+const toLower = v => v.toLowerCase();
+const contactSchema = mongoose.Schema({
+    firstName: { type: String, set: toLower },
+    lastName: { type: String, set: toLower },
+    phone: { type: String, set: toLower },
+    email: { type: String, set: toLower }
+});
+const Contact = mongoose.model('Contact', contactSchema);
+
+const addContact = contact => {
+    Contact.create(contact, err => {
+        assert.equal(null, err);
+        console.info('New contact added');
+        db.disconnect();
+    });
+};
+
+const getContact = name => {
+    const search = new RegExp(name, 'i');
+    Contact.find({$or: [{ firstName: search }, { lastName: search }]})
+    .exec((err, contact) => {
+        assert.equal(null, err);
+        console.info(contact);
+        console.info(`${contact.length} matches`); 
+        db.disconnect();
+    })
+}
+
+
